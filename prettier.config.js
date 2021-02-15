04af49bd7783706c516a7648e7024a53604d01b2
keithboice <69816300+keithboice@users.noreply.github.com>
2021-02-15 03:12:46 -0500

    Add files via upload

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 110,
+	endOfLine: "auto",
+	useTabs: true,
+	tabWidth: 4,
+	semi: true,
+	singleQuote: false,
+	quoteProps: "consistent",
+	vueIndentScriptAndStyle: true,
+	jsxSingleQuote: false,
+};
